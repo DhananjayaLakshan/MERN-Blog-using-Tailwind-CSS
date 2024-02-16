8b1f11c03d8cd96767398468f467981a309ae331
DhananjayaLakshan <abey.dhananjaya@gmail.com>
2024-02-16 18:08:25 +0530

    create signin route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs'
 import { errorHandler } from '../utils/error.js'
+import jwt from 'jsonwebtoken'
 
 
 export const signup = async (req, res, next) => {
@@ -18,9 +19,45 @@ export const signup = async (req, res, next) => {
     } catch (error) {
         next(error)
     }
+}
+
+export const signin = async (req, res, next) => {
+
+    const { email, password } = req.body
+
+    if (!email || !password || email === "" || password === "") {
+        next(errorHandler(400, 'All fields are required!'))
+    }
+
+    try {
+
+        const validUser = await User.findOne({ email })
+
+        if (!validUser) {
+            return next(errorHandler(404, 'Invalid User'))
+        }
+
+        const validPassword = bcrypt.compareSync(password, validUser.password)
+
+        if (!validPassword) {
+            return next(errorHandler(400, "Invalid Password"))
+        }
+
+        //create json web token
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+
+        //separate password from validUser object
+        const { password: pass, ...rest } = validUser._doc
+
+        //create cookie and send it with token
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true,
+        }).json(rest)
 
 
 
-
+    } catch (error) {
+        next(error)
+    }
 
 }
